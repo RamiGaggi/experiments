26de5977201f74cbd7fb076604be9f4c5b6cb418
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-08-09 00:52:09 +0300

    Experience with Selenium

---
+++ try-parsing/req.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions() 
+chrome_options.add_argument('--headless')
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
+try:
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://hh.ru/search/vacancy?area=1&fromSearchLine=true&st=searchVacancy&text=Python&from=suggest_post")
+    print(driver.title)
+
+except BaseException as e:
+    print(e)
+
+finally:
+    driver.close()
+
+# Список вакансий
+vacancies_list = driver.find_elements_by_xpath("//span[@class='g-user-content']/a[@class='bloko-link']")
