a7887c6e1c8d2d142ec3e8e1042a1904be1199cc
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-09-12 11:55:36 +0300

    Update

---
+++ try-async/select_2.py
@@ -0,0 +1,40 @@
+import socket
+import selectors
+
+selector = selectors.DefaultSelector()
+
+def server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('localhost', 5001))
+    server_socket.listen()
+
+
+def accept_connection(server_socket):
+    client_socket, addr = server_socket.accept()
+    print('Connetction from', addr)
+
+
+
+def send_meassage(client_socket):
+    request = client_socket.recv(4066)
+    if request:
+        response = b'Hello World!\n'
+        client_socket.sendall(response)
+    else:
+        client_socket.close()
+
+
+
+def event_loop():
+    while True:
+        ready_to_read, _, _ = select(monitor, [], [])  #  read, write, errors
+        
+        for socket in ready_to_read:
+            if socket is server_socket:
+                accept_connection(socket)
+            else:
+                send_meassage(socket)
+
+if __name__ == '__main__':
+    event_loop()
