4c287631b36718c545b89f7b8958af1767ee4cdf
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-09-17 10:56:35 +0300

    Update

--- try-async/select_2.py
+++ try-async/select_2.py
@@ -3,16 +3,29 @@ import selectors
 
 selector = selectors.DefaultSelector()
 
+
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('localhost', 5001))
     server_socket.listen()
+    
+    selector.register(
+        fileobj=server_socket,
+        events=selectors.EVENT_READ,
+        data=accept_connection,
+    )
 
 
 def accept_connection(server_socket):
     client_socket, addr = server_socket.accept()
     print('Connetction from', addr)
+    
+    selector.register(
+        fileobj=client_socket,
+        events=selectors.EVENT_READ,
+        data=send_meassage,
+    )
 
 
 
@@ -22,19 +35,17 @@ def send_meassage(client_socket):
         response = b'Hello World!\n'
         client_socket.sendall(response)
     else:
+        selector.unregister(client_socket)
         client_socket.close()
 
 
 
 def event_loop():
     while True:
-        ready_to_read, _, _ = select(monitor, [], [])  #  read, write, errors
-        
-        for socket in ready_to_read:
-            if socket is server_socket:
-                accept_connection(socket)
-            else:
-                send_meassage(socket)
+        event = selector.select()[0][0]
+        callback = event.data
+        callback(event.fileobj)
 
 if __name__ == '__main__':
+    server()
     event_loop()
